d34052cc20f2b5946efc35a56ef9c469e45d7418
Gorobchenko Anna <Agorobchenko@fnc-group.ru>
2024-10-26 21:04:54 +0300

    fix

--- task1.ts
+++ task1.ts
@@ -1,21 +1,21 @@
 type CalcFuncType = (a: number, b: number) => number;
-type ScaleType = 2 | 10 | 16;
+type ScaleType = 2 | 16;
 type CalcFuncNonDecimalType = (a: string, b: string, scale?: ScaleType) => string;
 
 let add: CalcFuncType = (a, b) => (a + b);
-let subtsracte: CalcFuncType = (a, b) => a - b;
-let multiplicate: CalcFuncType = (a, b) => a * b;
+let subtract: CalcFuncType = (a, b) => a - b;
+let multiply: CalcFuncType = (a, b) => a * b;
 let divide: CalcFuncType = (a, b) => a / b;
 
 let addNonDec: CalcFuncNonDecimalType = (a, b, scale = 2) => {
     let res = parseInt(a, scale) + parseInt(b, scale);
     return res.toString(scale);
 };
-let subtracteNonDec: CalcFuncNonDecimalType = (a, b, scale = 2) => {
+let subtractNonDec: CalcFuncNonDecimalType = (a, b, scale = 2) => {
     let res = parseInt(a, scale) - parseInt(b, scale);
     return res.toString(scale);
 };
-let multiplicateNonDec: CalcFuncNonDecimalType = (a, b, scale = 2) => {
+let multiplyNonDec: CalcFuncNonDecimalType = (a, b, scale = 2) => {
     let res = parseInt(a, scale) * parseInt(b, scale);
     return res.toString(scale);
 };
@@ -29,8 +29,8 @@ let a = 8;
 let b = 3;
 console.info('Decimal:');
 console.log(`a: ${a} + b: ${b} = ${add(a, b)}`);
-console.log(`a: ${a} - b: ${b} = ${subtsracte(a, b)}`);
-console.log(`a: ${a} * b: ${b} = ${multiplicate(a, b)}`);
+console.log(`a: ${a} - b: ${b} = ${subtract(a, b)}`);
+console.log(`a: ${a} * b: ${b} = ${multiply(a, b)}`);
 console.log(`a: ${a} / b: ${b} = ${divide(a, b)}`);
 
 //Binary
@@ -38,8 +38,8 @@ let aBin = '1100100'; //100
 let bBin = '10010'; //18
 console.info('Binary:');
 console.log(`a + b = ${aBin} + ${bBin} = ${addNonDec(aBin, bBin, 2)}`);
-console.log(`a - b = ${aBin} - ${bBin} = ${subtracteNonDec(aBin, bBin, 2)}`);
-console.log(`a * b = ${aBin} * ${bBin} = ${multiplicateNonDec(aBin, bBin, 2)}`);
+console.log(`a - b = ${aBin} - ${bBin} = ${subtractNonDec(aBin, bBin, 2)}`);
+console.log(`a * b = ${aBin} * ${bBin} = ${multiplyNonDec(aBin, bBin, 2)}`);
 console.log(`a / b = ${aBin} / ${bBin} = ${divideNonDec(aBin, bBin, 2)}`);
 
 //Hex
@@ -47,6 +47,6 @@ let aHex = '3E8'; //1000
 let bHex = '1F4'; //500
 console.info('Hex:');
 console.log(`a + b = ${aHex} + ${bHex} = ${addNonDec(aHex, bHex, 16)}`);
-console.log(`a - b = ${aHex} - ${bHex} = ${subtracteNonDec(aHex, bHex, 16)}`);
-console.log(`a * b = ${aHex} * ${bHex} = ${multiplicateNonDec(aHex, bHex, 16)}`);
+console.log(`a - b = ${aHex} - ${bHex} = ${subtractNonDec(aHex, bHex, 16)}`);
+console.log(`a * b = ${aHex} * ${bHex} = ${multiplyNonDec(aHex, bHex, 16)}`);
 console.log(`a / b = ${aHex} / ${bHex} = ${divideNonDec(aHex, bHex, 16)}`);
