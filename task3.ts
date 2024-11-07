c166c6ddcf83229d27f1672ec6c1e360c9dca741
Gorobchenko Anna <Agorobchenko@fnc-group.ru>
2024-11-07 14:37:54 +0300

    Task 3: Bank accounts

---
+++ task3.ts
@@ -0,0 +1,140 @@
+// Типы
+type DateNullable = Date | null | undefined;
+type canWithdrawFuncType = (a: number, b: number) => boolean;
+// --Типы
+  
+// Интерфейсы
+interface Account {
+  accountNumber: string, //номер счета
+  balance: number, //баланс
+  replenish(amount: number): void, //пополнение счета
+  withdraw(amount: number): void, //снятие средств
+  checkBalance(): void,
+}
+// --Интерфейсы
+
+// Вспомогательная функция для проверки возможности снятия средств
+const canWithdraw: canWithdrawFuncType = (amount: number, balance: number) => {
+  console.log(`Снять ${amount}р.`);
+  
+  if (amount <= 0) {
+    console.log('Введите корректную сумму для снятия.');
+    return false;
+  } else if (amount > balance) {
+    console.log(`Недостаточно средств.`);
+    return false;
+  } else {
+    return true;
+  }
+}
+// --
+
+
+// Классы, имплементирующие интерфейсы
+
+//Дебетовый счет
+class DebitAccount implements Account {
+  readonly accountNumber: string;
+  balance: number;
+  
+  constructor(accountNumber: string) {
+    this.accountNumber = accountNumber;
+    this.balance = 0;
+    console.log(`Создан счёт №${this.accountNumber}.`);
+  }
+
+  replenish(amount: number) {
+    if (amount <= 0) {
+      console.log('Введите корректную сумму');
+    } else {
+      this.balance = this.balance + amount;
+      console.log(`Вы пополнили счёт на ${amount}р.`);
+    }
+  }
+
+  withdraw(amount: number) {
+    const isAmountValid = canWithdraw(amount, this.balance);
+    
+    if (isAmountValid) {
+      this.balance = this.balance - amount;
+      console.log(`Вы сняли со счёта ${amount}р.`)
+    }
+  }
+
+  checkBalance() {
+    console.log(`Ваш баланс: ${this.balance}р.`)
+  }
+}
+
+const debitAccount = new DebitAccount('123456789');
+debitAccount.replenish(4000);
+debitAccount.checkBalance();
+debitAccount.withdraw(5000);
+debitAccount.withdraw(-5000);
+debitAccount.withdraw(500);
+debitAccount.checkBalance();
+
+// Кредитный счет
+class CreditAccount implements Account {
+  readonly accountNumber: string;  
+  private debt: number;
+  private nextPayment: DateNullable;
+  balance: number;
+
+  constructor(accountNumber: string, balance: number) {
+    this.accountNumber = accountNumber;
+    this.balance = balance;
+    this.debt = 0;
+    this.nextPayment = null;
+    console.log(`Создан счёт №${this.accountNumber}.`)
+  }
+
+  replenish(amount: number) {
+    if (amount <= 0) {
+      console.log('Введите корректную сумму');
+    } else {
+      this.balance = this.balance + amount;
+      this.debt = amount > this.debt ? 0 : this.debt - amount;
+    }
+    
+    if (this.nextPayment && !this.debt) this.nextPayment = null;
+
+    console.log(`Вы пополнили счёт на ${amount}р.`);
+  }
+
+  withdraw(amount: number) {
+    const isAmountValid = canWithdraw(amount, this.balance);    
+    if (!isAmountValid) return;
+    
+    this.balance = this.balance - amount;
+    this.debt = this.debt + amount;
+    
+    if (!this.nextPayment) {
+      const nextPayment = new Date();
+      nextPayment.setDate(nextPayment.getDate() + 50);
+      this.nextPayment = nextPayment;
+    }
+
+    console.log(`Вы сняли ${amount}р.`);
+  }
+
+  checkBalance() {
+    console.log(`Ваш баланс составляет ${this.balance}р.`);
+    
+    if (this.debt) {
+      console.log(`Ваша задолженность составляет ${this.debt}р.`);
+      console.log(`Необходимо оплатить долг до ${this.nextPayment?.toLocaleDateString()}`);
+    }
+  }
+}
+
+const creditAccount = new CreditAccount('543219876', 120000);
+creditAccount.withdraw(40000);
+creditAccount.checkBalance();
+creditAccount.withdraw(-40000);
+creditAccount.replenish(10000);
+creditAccount.checkBalance();
+creditAccount.replenish(30000);
+creditAccount.checkBalance();
+// --Классы, имплементирующие интерфейсы
+
